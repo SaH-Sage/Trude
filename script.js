27a21876bec016d433f4980af966ba792a04a69e
SaH-Sage <sondrealexander@hotmail.com>
2020-02-04 03:37:53 +0100

    Update function

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-burger.addEventListener("click", () => {
+function toggleNavMenu() {
   navLinks.classList.toggle("open");
 
   links.forEach(link => {
 	link.classList.toggle("fade");
   });
-})
+};
